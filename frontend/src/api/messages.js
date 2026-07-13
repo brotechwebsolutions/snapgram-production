b86f59fcf41ebe0220d3fe93d2e95499d7f2c367
brotechwebsolutions <brotechinformation@gmail.com>
2026-07-13 15:05:16 +0530

    Update messages.js

--- frontend/src/api/messages.js
+++ frontend/src/api/messages.js
@@ -5,6 +5,7 @@ export const messagesApi = {
   getConversations:  ()                      => api.get('/messages/conversations'),
   getMessages:       (convId, page, size)    => api.get(`/messages/conversations/${convId}?page=${page}&size=${size}`),
   sendTyping:        (convId, isTyping)      => api.post(`/messages/conversations/${convId}/typing`, { isTyping }),
+  editMessage:       (messageId, content)    => api.put(`/messages/${messageId}`, { content }),
   markSeen:          (messageId)             => api.post(`/messages/${messageId}/seen`),
   deleteMessage:     (messageId)             => api.delete(`/messages/${messageId}`),
   pinConversation:   (convId)               => api.post(`/messages/conversations/${convId}/pin`),
